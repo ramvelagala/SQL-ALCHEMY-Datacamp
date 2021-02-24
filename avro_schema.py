f80fd7b05f1235c00667cac7da2d622434af748a
Ram <kittuvelagala@gmail.com>
2021-02-24 09:24:57 -0600

    added avro schema

---
+++ avro_schema.py
@@ -0,0 +1,74 @@
+import json
+from tsting import format_generation
+
+print(format_generation())
+
+def avro_schema():
+
+
+    sample = {
+        "type": "record",
+        "name": "userInfo",
+        "namespace": "my.example",
+        "fields": [{"name": "Definition",
+                    "type": {
+                        "name": "Definition",
+                        "type": "record",
+                        "fields": [
+                            {"name": "Metadata",
+                             "type":"string",
+                             "default" : None ,
+                             "Values"  :
+                             },
+                            {"name": "Data Type",
+                             "type": "int",
+                             "default": None},
+                            {"name": "Nullability",
+
+                             },
+                            "default": None},
+    "type": "boolean",
+    "default": None},
+
+    ]
+
+
+    {"name": "age",
+     "type": "int",
+     "default": -1},
+
+    {"name": "phone",
+     "type": "string",
+     "default": None},
+
+    {"name": "housenum",
+     "type": "string",
+     "default": None},
+
+    {"name": "Domain Highlights",
+     "type": {
+         "type": "record",
+         "name": "Domain Highlights",
+         "fields": [
+             {"name": "Total Rows",
+              "type": "int",
+              "default": None},
+
+             {"name": "Cardinality",
+              "type": "int",
+              "default": None},
+
+             {"name": "Cardninality %",
+              "type": "float",
+              "default": None},
+
+         ]},
+     "default": {}
+     }
+    ]
+    }
+    data = json.dumps(sample)
+    print(data)
+    return data
+
+print(avro_schema())
