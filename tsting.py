48462e409ff07d029392d6018fd84571ac303a73
Ram <kittuvelagala@gmail.com>
2021-02-24 20:59:50 -0600

    avro added

--- tsting.py
+++ tsting.py
@@ -1,28 +1,97 @@
-import re
-
-
-def format_generation():
-    data1 = [1, -2, 3.0, -4.0, -5.1, 6.2, 7.369, -8.356, 4565242, -123.7778888]
-    data = [1, 23, 456, -7891]
-    res_list = []
-    data1 = [str(num) for num in data1]
-    for num in data1:
-        a = num.split('.')
-        x = ''
-        y = ''
-        dd = re.sub(r'[^\w]', '', a[0])
-        x = re.sub(r"\d+", len(dd) * '9', dd)
-        # print(x)
-        if len(a) == 2:
-            y = re.sub(r"\d+", len(a[1]) * '9', dd)
-            y = '.' + y
-        else:
-            pass
-        if a[0].startswith('-'):
-            x = '-' + x
-        res = x + y
-        res_list.append(res)
-    return data1, res_list
-
-
-print(format_generation())
+{
+    "name": "Report",
+    "type": "record",
+    "fields": [
+        {
+            "name": "Column",
+            "type": "string"
+        },
+        {
+            "name": "domain_highlights",
+            "type": {
+                "name": "domain_highlights",
+                "type": "record",
+                "fields": [
+                    {
+                        "name": "TotalRows",
+                        "type": "int"
+                    },
+                    {
+                        "name": "Cardinality",
+                        "type": "int"
+                    },
+                    {
+                        "name": "per_of_Cardinality",
+                        "type": "int"
+                    },
+                    {
+                        "name": "Nulls",
+                        "type": "int"
+                    },
+                    {
+                        "name": "no_of_general_formats",
+                        "type": "int"
+                    },
+                    {
+                        "name": "general_formats",
+                        "type": {
+                            "type": "array",
+                            "items": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            "name": "MetaData",
+            "type": {
+                "name": "MetaData",
+                "type": "record",
+                "fields": [
+                    {
+                        "name": "Inferred",
+                        "type": {
+                            "name": "Inferred",
+                            "type": "record",
+                            "fields": [
+                                {
+                                    "name": "Data_type",
+                                    "type": "string"
+                                },
+                                {
+                                    "name": "data_lenght",
+                                    "type": "int"
+                                },
+                                {
+                                    "name": "Precision",
+                                    "type": "int"
+                                },
+                                {
+                                    "name": "scale",
+                                    "type": "int"
+                                },
+                                {
+                                    "name": "data_scale",
+                                    "type": "int"
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        "name": "Defined",
+                        "type": {
+                            "name": "Defined",
+                            "type": "record"
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            "name": "inferred_data_class",
+            "type": "string"
+        }
+    ]
+}
